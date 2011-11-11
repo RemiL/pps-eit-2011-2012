99f50e2678ea26bbcb59456a0e248a8e4914cdb2
remi.lacroix <remi.lacroix@0ad28bcd-e553-0f2e-5dc1-b2db4284b036>
2011-11-11 19:33:14 +0000

    Correction (hasardeuse ...) des warnings générés par le package Snowball.

--- src/tools/org/tartarus/snowball/SnowballStemmer.java
+++ src/tools/org/tartarus/snowball/SnowballStemmer.java
@@ -1,6 +1,5 @@
 
 package tools.org.tartarus.snowball;
-import java.lang.reflect.InvocationTargetException;
 
 public abstract class SnowballStemmer extends SnowballProgram {
     public abstract boolean stem();
