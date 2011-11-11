29df1a823bfc71eb85754f3c5d59135aeca4e25a
groupeCMT07@gmail.com <groupeCMT07@gmail.com@0ad28bcd-e553-0f2e-5dc1-b2db4284b036>
2011-11-11 18:17:16 +0000

    - Remplissage de la classe Document

--- src/index/Document.java
+++ src/index/Document.java
@@ -1,5 +1,51 @@
 package index;
 
 public class Document {
-
+	
+	/** L'url du document */
+	private String url;
+	/** Le titre du document */
+	private String titre;
+	
+	/**
+	 * Construit un document avec un titre et une url
+	 * @param url l'url du document
+	 * @param title le titre du document
+	 */
+	public Document(String url, String title) {
+		this.url = url;
+		this.titre = title;
+	}
+	
+	/**
+	 * Retourne l'url du document
+	 * @return l'url du document
+	 */
+	public String getUrl() {
+		return url;
+	}
+	
+	/**
+	 * Modifie l'url du document
+	 * @param url la nouvelle url du document
+	 */
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+	/**
+	 * Retourne le titre du document
+	 * @return le titre du document
+	 */
+	public String getTitre() {
+		return titre;
+	}
+	
+	/**
+	 * Modifie le titre du document
+	 * @param titre le nouveau titre du document
+	 */
+	public void setTitre(String title) {
+		this.titre = title;
+	}
 }
