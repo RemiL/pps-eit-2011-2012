d7227a6172028dbbad5405c9fbd339107b47a18d
remi.lacroix <remi.lacroix@0ad28bcd-e553-0f2e-5dc1-b2db4284b036>
2011-11-11 19:28:15 +0000

    Implémentation des normalisateurs de base :  - Normalizer : transformée en classe mère abstraite pour centraliser la gestion des mots vides.  - FrenchTokenizer (segmentation) et ajout de son automate FrenchTokenizerAutomaton.  - FrenchStemmer (racinisation) et ajout du package Snowball.

--- trunk/src/tools/normalizer/FrenchStemmer.java
+++ trunk/src/tools/normalizer/FrenchStemmer.java
@@ -1,5 +1,74 @@
 package tools.normalizer;
 
-public class FrenchStemmer implements Normalizer {
+import java.io.IOException;
+import java.util.ArrayList;
 
+import tools.org.tartarus.snowball.ext.frenchStemmer;
+
+/**
+ * Classe implémentant un normalisateur basé sur la racinisation des mots en
+ * français (cf le package SnowBall http://snowball.tartarus.org/download.php).
+ */
+public class FrenchStemmer extends FrenchTokenizer {
+	private static short REPEAT = 1;
+	/** Le raciniseur de base. */
+	private frenchStemmer stemmer;
+
+	/**
+	 * Construit un normalisateur de texte basé sur la racinisation des unités
+	 * lexicales.
+	 */
+	public FrenchStemmer() {
+		super();
+		stemmer = new frenchStemmer();
+	}
+
+	/**
+	 * Construit un normalisateur de texte basé sur la racinisation des unités
+	 * lexicales. La liste de mots vides est initialisée à partir du fichier
+	 * fourni. Ce fichier doit contenir un mot vide par ligne.
+	 * 
+	 * @param stopWordFileName
+	 *            le nom du fichier contenant les mots vides
+	 * @param encoding
+	 *            l'encodage de caractères utilisé pour le fichier contenant les
+	 *            mots vides
+	 * @throws IOException
+	 *             si la lecture du fichier dont le nom est fourni échoue
+	 */
+	public FrenchStemmer(String stopWordFileName, String encoding)
+			throws IOException {
+		super(stopWordFileName, encoding);
+		stemmer = new frenchStemmer();
+	}
+
+	/**
+	 * Renvoie la liste d'unités lexicales contenues dans le texte spécifié en
+	 * appliquant une racinisation des mots (avec ou sans suppression des mots
+	 * vides).
+	 * 
+	 * @param text
+	 *            le texte à normaliser
+	 * @param removeStopWords
+	 *            indique si les mots vides doivent être supprimés par la
+	 *            normalisation
+	 * @return la liste d'unités lexicales contenues dans le texte spécifié en
+	 *         appliquant une racinisation des mots
+	 */
+	public ArrayList<String> normalize(String text, boolean removeStopWords) {
+		// On segmente le texte
+		ArrayList<String> words = super.normalize(text, removeStopWords);
+
+		// puis on racinise.
+		ArrayList<String> result = new ArrayList<String>();
+		for (String word : words) {
+			stemmer.setCurrent(word);
+			for (int i = REPEAT; i != 0; i--) {
+				stemmer.stem();
+			}
+			result.add(stemmer.getCurrent());
+		}
+
+		return result;
+	}
 }
--- trunk/src/tools/normalizer/Normalizer.java
+++ trunk/src/tools/normalizer/Normalizer.java
@@ -1,5 +1,81 @@
 package tools.normalizer;
 
-public interface Normalizer {
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashSet;
 
+/**
+ * Classe définissant un normalisateur abstrait de texte capable de gérer la
+ * suppression des mots vides.
+ */
+public abstract class Normalizer {
+	/** Liste des mots vides */
+	private HashSet<String> stopWords;
+
+	/**
+	 * Construit un normalisateur de texte dont la liste de mots vides est vide.
+	 */
+	public Normalizer() {
+		stopWords = new HashSet<String>();
+	}
+
+	/**
+	 * Construit un normalisateur de texte dont la liste de mots vides est
+	 * initialisée à partir du fichier fourni. Ce fichier doit contenir un mot
+	 * vide par ligne.
+	 * 
+	 * @param stopWordFileName
+	 *            le nom du fichier contenant les mots vides
+	 * @param encoding
+	 *            l'encodage de caractères utilisé pour le fichier contenant les
+	 *            mots vides
+	 * @throws IOException
+	 *             si la lecture du fichier dont le nom est fourni échoue
+	 */
+	public Normalizer(String stopWordFileName, String encoding)
+			throws IOException {
+		stopWords = new HashSet<String>();
+
+		// lecture du fichier texte
+		InputStream ips = new FileInputStream(stopWordFileName);
+		InputStreamReader ipsr = new InputStreamReader(ips, encoding);
+		BufferedReader br = new BufferedReader(ipsr);
+		String line;
+		while ((line = br.readLine()) != null) {
+			this.stopWords.add(line);
+		}
+		br.close();
+		ipsr.close();
+		ips.close();
+	}
+
+	/**
+	 * Teste si un mot est un mot vide.
+	 * 
+	 * @param word
+	 *            le mot à tester
+	 * @return vrai si et seulement si le mot fourni est un mot vide, sinon faux
+	 */
+	protected boolean isStopWord(String word) {
+		return stopWords.contains(word);
+	}
+
+	/**
+	 * Renvoie la liste d'unités lexicales contenues dans le texte spécifié en
+	 * appliquant une normalisation.
+	 * 
+	 * @param text
+	 *            le texte à normaliser
+	 * @param removeStopWords
+	 *            indique si les mots vides doivent être supprimés par la
+	 *            normalisation
+	 * @return la liste d'unités lexicales contenues dans le texte spécifié en
+	 *         appliquant une normalisation
+	 */
+	public abstract ArrayList<String> normalize(String text,
+			boolean removeStopWords);
 }
