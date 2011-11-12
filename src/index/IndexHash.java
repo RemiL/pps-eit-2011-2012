9152ff0385d36f0e98e587ad64bfb476443fab2b
groupeCMT07@gmail.com <groupeCMT07@gmail.com@0ad28bcd-e553-0f2e-5dc1-b2db4284b036>
2011-11-12 03:48:23 +0000

    - Ajout du diagramme de classe mis à jour - Création de la classe IndexHash qui hérite de la classe abstraite Index - Création de la classe CoupleOccurrencePoids qui contient le couple nombre d'occurrences dans le document/poids - Remplissage de la classe Index - Remplissage de la classe IndexHash

---
+++ src/index/IndexHash.java
@@ -0,0 +1,58 @@
+package index;
+
+import java.util.HashMap;
+
+public class IndexHash extends Index {
+
+	HashMap<String, HashMap<String, CoupleOccurrencePoids>> index;
+	
+	/**
+	 * Construit un index vide
+	 */
+	public IndexHash()
+	{
+		super();
+		index = new HashMap<String, HashMap<String, CoupleOccurrencePoids>>();
+	}
+
+	@Override
+	protected void addTerme(String terme) {
+		index.put(terme, new HashMap<String, CoupleOccurrencePoids>());
+	}
+
+	@Override
+	public void addDocumentTerme(String terme, Document document) {
+		
+		if(!index.containsKey(terme))
+			addTerme(terme);
+		if(!listeDocuments.containsKey(document.getUrl()))
+			addDocument(document);
+			
+		HashMap<String, CoupleOccurrencePoids> liste = index.get(terme);
+			
+		if(liste.containsKey(document.getUrl()))
+			liste.get(document.getUrl()).setNbOccurrences(liste.get(document.getUrl()).getNbOccurrences() + 1);
+		else
+			liste.put(document.getUrl(), new CoupleOccurrencePoids(1, 0));		
+	}
+
+	@Override
+	public int getNbDocumentsTerme(String terme) {
+		return index.get(terme).size();
+	}
+
+	@Override
+	public int getNbOccurrencesTermeDocument(String terme, String urlDocument) {
+		return index.get(terme).get(urlDocument).getNbOccurrences();
+	}
+
+	@Override
+	public double getPoids(String terme, String urlDocument) {
+		return index.get(terme).get(urlDocument).getPoids();
+	}
+
+	@Override
+	public void setPoids(String terme, String urlDocument, int poids) {
+		index.get(terme).get(urlDocument).setPoids(poids);
+	}
+}
