068c02c89046752c322b3c107e32128f341ab56d
groupeCMT07@gmail.com <groupeCMT07@gmail.com@0ad28bcd-e553-0f2e-5dc1-b2db4284b036>
2011-11-12 03:48:23 +0000

    - Ajout du diagramme de classe mis à jour - Création de la classe IndexHash qui hérite de la classe abstraite Index - Création de la classe CoupleOccurrencePoids qui contient le couple nombre d'occurrences dans le document/poids - Remplissage de la classe Index - Remplissage de la classe IndexHash

---
+++ trunk/src/index/CoupleOccurrencePoids.java
@@ -0,0 +1,49 @@
+package index;
+
+public class CoupleOccurrencePoids {
+	
+	private int nbOccurrences;
+	private double poids;
+	
+	/**
+	 * Construit un couple nombre d'occurrences/poids avec les valeurs passées en paramètre
+	 * @param nbOccurrences le nombre d'occurrences du terme dans le document
+	 * @param poids le poids du document
+	 */
+	public CoupleOccurrencePoids(int nbOccurrences, double poids) {
+		this.nbOccurrences = nbOccurrences;
+		this.poids = poids;
+	}
+
+	/**
+	 * Retourne le nombre d'occurrences du terme dans le document
+	 * @return le nombre d'occurrences
+	 */
+	public int getNbOccurrences() {
+		return nbOccurrences;
+	}
+
+	/**
+	 * Modifie le nombre d'occurrence du terme dans le document
+	 * @param nbOccurrences le nouveau nombre d'occurrence du terme dans le document
+	 */
+	public void setNbOccurrences(int nbOccurrences) {
+		this.nbOccurrences = nbOccurrences;
+	}
+
+	/**
+	 * Retourne le poids
+	 * @return le poids
+	 */
+	public double getPoids() {
+		return poids;
+	}
+
+	/**
+	 * Modifie le poids
+	 * @param poids le nouveau poids
+	 */
+	public void setPoids(double poids) {
+		this.poids = poids;
+	}	
+}
--- trunk/src/index/Index.java
+++ trunk/src/index/Index.java
@@ -1,5 +1,68 @@
 package index;
 
-public class Index {
+import java.util.HashMap;
 
+public abstract class Index {
+	
+	/** Table de hashage qui à une url associe un document */
+	protected HashMap<String, Document> listeDocuments;
+	
+	/**
+	 * Construit un Index vide
+	 */
+	public Index()
+	{
+		listeDocuments = new HashMap<String, Document>();
+	}
+	
+	/**
+	 * Ajoute un document à la liste de documents
+	 * @param document le document à ajouter
+	 */
+	protected void addDocument(Document document)
+	{
+		listeDocuments.put(document.getTitre(), document);
+	}
+	
+	/** 
+	 * Ajoute un terme à l'index
+	 * @param terme le terme à ajouter à l'index
+	 */
+	protected abstract void addTerme(String terme);
+	
+	/**
+	 * Ajoute un document à un terme. S'il existe déjà, son nombre d'occurrence est incrémenté. Sinon le document est créé
+	 * @param terme le terme auquel ajouter le document
+	 * @param document le document à ajouter au terme
+	 */
+	public abstract void addDocumentTerme(String terme, Document document);
+	
+	/**
+	 * Modifie le poids d'un terme d'un document
+	 * @param terme le terme
+	 * @param urlDocument l'url du document
+	 */
+	public abstract void setPoids(String terme, String urlDocument, int poids);
+	
+	/**
+	 * Retourne le nombre d'occurrences du terme dans le document
+	 * @param terme le terme voulu
+	 * @param urlDocument l'url du document
+	 */
+	public abstract int getNbOccurrencesTermeDocument(String terme, String urlDocument);
+	
+	/**
+	 * Retourne le nombre de documents dans lesquels ce trouve le terme
+	 * @param terme le terme
+	 * @return le nombre de documents dans lesquels ce trouve le terme
+	 */
+	public abstract int getNbDocumentsTerme(String terme);
+	
+	/**
+	 * Retourne le poids d'un terme dans un document
+	 * @param terme le terme
+	 * @param urlDocument l'url du document
+	 * @return le poids d'un terme dans un document
+	 */
+	public abstract double getPoids(String terme, String urlDocument);
 }
