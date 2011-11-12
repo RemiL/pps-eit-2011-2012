060ea79fbc5baf2c8b20d64b99d2cf30b634f53d
remi.lacroix <remi.lacroix@0ad28bcd-e553-0f2e-5dc1-b2db4284b036>
2011-11-12 12:48:44 +0000

    Ajout d'un indexer basique pour les fichiers au format texte :  - implémentation de Indexer et IndexerTexte.  - ajout d'un constructeur par défaut pour Document.

--- src/index/Indexer.java
+++ src/index/Indexer.java
@@ -1,5 +1,35 @@
 package index;
 
-public abstract class Indexer {
+import java.io.IOException;
+import java.util.ArrayList;
 
+import tools.normalizer.Normalizer;
+import tools.ponderateur.Ponderateur;
+
+// TODO : transformer cette classe en interface ?
+public abstract class Indexer {
+	/**
+	 * Indexe les documents dont la liste est fournie pour remplir l'index. Le
+	 * normalisateur et le pondérateur fournis sont utilisés et les mots vides
+	 * sont éventuellement supprimés si la demande en est faite.
+	 * 
+	 * @param docsList
+	 *            la liste des documents à indexer
+	 * @param encoding
+	 *            l'encodage des documents à indexer
+	 * @param index
+	 *            l'index à remplir
+	 * @param normalizer
+	 *            le normalisateur à utiliser
+	 * @param removeStopWords
+	 *            indique si les mots vides doivent être supprimés par la
+	 *            normalisation
+	 * @param ponderateur
+	 *            le pondérateur à utiliser
+	 * @throws IOException
+	 *             si la lecture d'un document échoue
+	 */
+	public abstract void index(ArrayList<String> docsList, String encoding,
+			Index index, Normalizer normalizer, boolean removeStopWords,
+			Ponderateur ponderateur) throws IOException;
 }
