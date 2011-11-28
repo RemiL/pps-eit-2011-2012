432626cf616918b1fad1a6096783397c40c91003
remi.lacroix <remi.lacroix@0ad28bcd-e553-0f2e-5dc1-b2db4284b036>
2011-11-28 16:36:47 +0000

    Ajout de deux méthodes permettant de centraliser la sauvegarde et le chargement d'index vers ou depuis un fichier.

--- src/searcher/SearcherMain.java
+++ src/searcher/SearcherMain.java
@@ -2,10 +2,7 @@ package searcher;
 
 import index.Index;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
 
 import tools.normalizer.FrenchTokenizer;
 import tools.weigher.WeigherTfIdf;
@@ -18,31 +15,26 @@ public class SearcherMain {
 	public static void main(String[] args) {
 		try {
 			long t1 = System.nanoTime();
-			// Lecture de l'index depuis un fichier
-			FileInputStream fis = new FileInputStream("indexSansMotsVides.ser");
-			BufferedInputStream bis = new BufferedInputStream(fis);
-			ObjectInputStream ois = new ObjectInputStream(bis);
-			Index index = (Index) ois.readObject();
+			// Chargement de l'index depuis un fichier
+			Index index = Index.load("indexSansMotsVides.ser");
 
 			long t2 = System.nanoTime();
-			System.out.println("Temps de désérialisation :" + (t2 - t1) / 1000000.);
+			System.out.println("Temps de désérialisation : " + (t2 - t1) / 1000000.);
 
 			// Création du searcher
 			Searcher searcher = new SearcherVectorModel(new FrenchTokenizer("frenchST.txt", "UTF-8"),
 					new WeigherTfIdf(), index);
 
 			long t3 = System.nanoTime();
-			System.out.println("Temps de création du searcher :" + (t3 - t2) / 1000000.);
+			System.out.println("Temps de création du searcher : " + (t3 - t2) / 1000000.);
 
 			for (Result res : searcher.search("Basketball encyclopédie guide", true, Searcher.ALL_RESULTS)) {
 				System.out.println(" - " + res.getDocument().getUrl() + " (" + res.getPertinence() + ")");
 			}
 
 			long t4 = System.nanoTime();
-			System.out.println("Temps de recherche :" + (t4 - t3) / 1000000.);
+			System.out.println("Temps de recherche : " + (t4 - t3) / 1000000.);
 		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (ClassNotFoundException e) {
 			e.printStackTrace();
 		}
 	}
