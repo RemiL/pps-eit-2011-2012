7ef21c9d805f8c4b13c821c5c15ea6a873e6b994
remi.lacroix <remi.lacroix@0ad28bcd-e553-0f2e-5dc1-b2db4284b036>
2011-12-02 22:25:35 +0000

    Correction : le calcul du Tf.Idf utilisait par erreur une division entière ...

--- trunk/src/tools/weigher/WeigherTfIdf.java
+++ trunk/src/tools/weigher/WeigherTfIdf.java
@@ -14,12 +14,12 @@ public class WeigherTfIdf implements Weigher {
 	@Override
 	public double calculateWeight(String term, Document document, Index index) {
 		return index.getNbOccurrencesTermDocument(term, document)
-				* Math.log10(index.getNbDocuments() / index.getNbDocumentsTerm(term));
+				* Math.log10((double) index.getNbDocuments() / index.getNbDocumentsTerm(term));
 	}
 
 	@Override
 	public double calculateWeight(String word, List<String> wordsDoc, Index index) {
 		return Collections.frequency(wordsDoc, word)
-				* Math.log10(index.getNbDocuments() / index.getNbDocumentsTerm(word));
+				* Math.log10((double) index.getNbDocuments() / index.getNbDocumentsTerm(word));
 	}
 }
